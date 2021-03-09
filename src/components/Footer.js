2e9e410c1cec1965eb14eb0c199482b5588b350a
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2021-03-09 00:23:58 -0600

    theme, separate burger and menu components, footer, began integrating layout

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <footer className="bg-white text-center">
+        <p className="">&copy; Samantha Maka Photography {new Date().getFullYear()}</p>
+      </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Burger from './Burger';
+import Menu from './Menu';
+import Footer from './Footer';
+import { BurgerHook } from '../hooks/BurgerHook';
+
+
+
+const Layout = ({ children }) => {
+    return (
+
+    );
+}
+
+export default Layout;

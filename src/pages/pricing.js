0f1ac9ffe38c288691f32566fd0f3fe4eb424bf7
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2021-03-15 18:38:42 -0500

    began fixingup pricing page

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -2,9 +2,20 @@ import React from "react";
 
 const PricingPage = () => {
     return (
-        <>
-            <h1>Pricing Page</h1>
-        </>
+        <section className="h-screen w-full" style={{backgroundColor: '#C48F7F'}}>
+            <div className="w-11/12 relative max-w-screen-md mx-auto" style={{backgroundColor: 'rgba(255,255,255, .7)'}}>
+                <h1 className="text-3xl text-center" style={{color: '#3D3D3D'}}>Pricing</h1>
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+            </div>
+            <div className="">
+                
+            </div>
+        </section>
     );
 }
 
